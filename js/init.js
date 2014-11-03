a0953735c55f73dc01598447c1573bf895538ac5
buschidos <sergey.snozyk@gmail.com>
2014-11-03 16:28:09 +0200

    fix for menu

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load("js/lib/jquery.js",
-					"js/lib/modernizr.custom.js",
-					"js/lib/flexmenu.js",
-					"js/lib/slick.js",
+					// "js/lib/modernizr.custom.js",
+					// "js/lib/flexmenu.js",
+          "js/lib/slick.js",
+					"js/lib/menuFlex.js",
 					"js/common.js");
--- js/min/common-min.js
+++ js/min/common-min.js
@@ -5,17 +5,17 @@ head.ready(function() {
 		$('.js-side-menu').removeClass('is-visible');
 	});
 
-	$('.menu-flex').flexMenu({
-		'threshold' : 1, // [integer] If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
-		'cutoff' : 0, // [integer] If there is space for this many or fewer items outside our "more" popup, just move everything into the more menu. In that case, also use linkTextAll and linkTitleAll instead of linkText and linkTitle. To disable this feature, just set this value to 0.
-		'linkText' : '•••', // [string] What text should we display on the "view more" link?
-		'linkTitle' : '', // [string] What should the title of the "view more" button be?
-		'linkTextAll' : 'Меню', // [string] If we hit the cutoff, what text should we display on the "view more" link?
-		'linkTitleAll' : '', // [string] If we hit the cutoff, what should the title of the "view more" button be?
-		'showOnHover' : true, // [boolean] Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
-		'popupAbsolute' : true, // [boolean] Should we absolutely position the popup? Usually this is a good idea. That way, the popup can appear over other content and spill outside a parent that has overflow: hidden set. If you want to do something different from this in CSS, just set this option to false.
-		'undo' : false // [boolean] If this is true, we'll move the list items back to where they were before, and remove the "View More" link. This is useful if you actually do want list items to stack in some cases, or if you want to recalculate the menu.
-	});
+	// $('.menu-flex').flexMenu({
+	// 	'threshold' : 1, // [integer] If there are this many items or fewer in the list, we will not display a "View More" link and will instead let the list break to the next line. This is useful in cases where adding a "view more" link would actually cause more things to break  to the next line.
+	// 	'cutoff' : 0, // [integer] If there is space for this many or fewer items outside our "more" popup, just move everything into the more menu. In that case, also use linkTextAll and linkTitleAll instead of linkText and linkTitle. To disable this feature, just set this value to 0.
+	// 	'linkText' : '•••', // [string] What text should we display on the "view more" link?
+	// 	'linkTitle' : '', // [string] What should the title of the "view more" button be?
+	// 	'linkTextAll' : 'Меню', // [string] If we hit the cutoff, what text should we display on the "view more" link?
+	// 	'linkTitleAll' : '', // [string] If we hit the cutoff, what should the title of the "view more" button be?
+	// 	'showOnHover' : true, // [boolean] Should we we show the menu on hover? If not, we'll require a click. If we're on a touch device - or if Modernizr is not available - we'll ignore this setting and only show the menu on click. The reason for this is that touch devices emulate hover events in unpredictable ways, causing some taps to do nothing.
+	// 	'popupAbsolute' : true, // [boolean] Should we absolutely position the popup? Usually this is a good idea. That way, the popup can appear over other content and spill outside a parent that has overflow: hidden set. If you want to do something different from this in CSS, just set this option to false.
+	// 	'undo' : false // [boolean] If this is true, we'll move the list items back to where they were before, and remove the "View More" link. This is useful if you actually do want list items to stack in some cases, or if you want to recalculate the menu.
+	// });
 
 	$('.menu__btn_search').on('click', function() {
 		$('.header__search').toggleClass('is-visible');
@@ -98,6 +98,7 @@ head.ready(function() {
 		]
 	});
 
+
 	//toggle gallery autoplay
 	var galleryAutoplayStatus = 0;
 	$('.gallery__carousel-autoplay').on('click', function(event){
@@ -112,6 +113,51 @@ head.ready(function() {
 		};
 	});
 
+	// menuFlex('.menu-flex');
+	$('.menu-flex').menuFlex();
 });
 
+(function($) {
+
+	$.fn.menuFlex = function() {
+
+		var _this = this;
+
+		_this.each(function(index, el) {
+			console.log(_this);
+
+			// console.log(_this);
+			// console.log(_this.children().length);
+			var menuHTML  = _this.html();
+			var menuWidth = _this.width();
+			var menuItems = _this.children();
+			console.log(menuWidth);
+			console.log(menuItems.length);
+
+			var itemsWidth = 0;
+
+			for (var i = 0; i <= menuItems.length - 1; i++) {
+				itemsWidth += menuItems[i].offsetWidth;
+			};
+
+			for ( i = menuItems.length - 1; i >= 0; i--) {
+				if (itemsWidth > menuWidth) {
+					itemsWidth -= menuItems[i].offsetWidth;
+					_this.find('li:last-child').appendTo('.menuMore').remove();
+					console.log('wow');
+				} else {
+					break;
+				};
+			};
+
+			console.log(itemsWidth);
+			console.log(menuItems.length);
+
+
+
+		});
+
+	};
+
+})(jQuery);
 
